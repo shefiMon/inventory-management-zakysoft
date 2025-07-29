0cb34e7aed1ff9a6ebf1c01040f3878a51ef7ef7
shefiMon <shafeeqaklkt@gmail.com>
2025-07-30 00:45:54 +0530

    route updated

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 
 Route::prefix('v1')->group(function () {
-    // Define your API routes here
 
     Route::prefix('auth')->group(function () {
         Route::post('/login', [\App\Http\Controllers\Auth\ApiAuthController::class, 'store']);
